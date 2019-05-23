e8604630cf720d1dfb1dcee75ff949f01565774d
Заборских <717-1_zaf@36411.cloud.hosted-by-virtualdc.ru>
2019-05-23 18:03:37 +0700

    Первая Практика

---
+++ 717-1_zaf-1-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+#include <math.h> 
+int main() 
+{ 
+int a; 
+int b; 
+int res; 
+scanf( "%d", &a ); 
+scanf( "%d", &b ); 
+res = pow(a,b); 
+printf("%d\n", res); 
+return 0; 
+}
